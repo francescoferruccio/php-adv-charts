f84ee927cfc164f6f3076481979a7ce77b4dbf43
Francesco <francesco.ferruccio88@gmail.com>
2020-05-11 12:13:41 +0200

    aggiunto stile grafico come da consegna

--- main.js
+++ main.js
@@ -15,22 +15,16 @@ $(document).ready(function() {
                   label: 'Vendite',
                   data: data,
                   backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)'
+                      'green'
                   ],
                   borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)'
+                      'red'
                   ],
-                  borderWidth: 1
+                  borderWidth: 3,
+                  pointBackgroundColor: 'green',
+                  pointBorderColor: 'red',
+                  pointBorderWidth: 2,
+                  pointRadius: 4
               }]
           },
           options: {
