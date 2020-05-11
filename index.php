d5e105b180cef93a45d8ee699dfce92a468708e4
Francesco <francesco.ferruccio88@gmail.com>
2020-05-11 11:35:22 +0200

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Chart JS</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="main.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
