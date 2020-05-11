314bb6d11dc92dd4b92cba4b25fde2196b699aa7
Francesco <francesco.ferruccio88@gmail.com>
2020-05-11 12:04:52 +0200

    funzionamento base grafico

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>Chart JS</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2/dist/Chart.min.js"></script>
     <script src="main.js"></script>
+    <link rel="stylesheet" href="style.css">
   </head>
-  <body>
 
+  <body>
+    <div class="container">
+      <!-- canvas chartjs -->
+      <canvas id="lineChart"></canvas>
+    </div>
   </body>
 </html>
