cc68b943e1ef8868315cce7349195e300adbb9ac
Francesco <francesco.ferruccio88@gmail.com>
2020-05-11 12:29:46 +0200

    refactoring - creata funzione per la stampa della chart

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
+// funzione che stampa una Line Chart di chartjs
+function printLineChart(target, labels, data) {
+  var ctx = target;
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Vendite',
+            data: data,
+            backgroundColor: 'green',
+            borderColor: 'red',
+            borderWidth: 3,
+            pointBackgroundColor: 'green',
+            pointBorderColor: 'red',
+            pointBorderWidth: 2,
+            pointRadius: 4
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+  });
+}
+
 $(document).ready(function() {
+  var lineCanvas = $('#lineChart');
+  var mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
 
   $.ajax({
     url: 'server.php',
     method: 'GET',
     success: function(data) {
-      // console.log(data);
-
-      var ctx = $('#lineChart');
-      var myChart = new Chart(ctx, {
-          type: 'line',
-          data: {
-              labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
-              datasets: [{
-                  label: 'Vendite',
-                  data: data,
-                  backgroundColor: [
-                      'green'
-                  ],
-                  borderColor: [
-                      'red'
-                  ],
-                  borderWidth: 3,
-                  pointBackgroundColor: 'green',
-                  pointBorderColor: 'red',
-                  pointBorderWidth: 2,
-                  pointRadius: 4
-              }]
-          },
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero: true
-                      }
-                  }]
-              }
-          }
-      });
+      printLineChart(lineCanvas, mesi, data);
     },
     error: function(err) {
       console.error("ERRORE", err);
